19a4bc414ddc7a956c5568a647a8b0db2e31ef42
Kanwal Shahzadi <160924893+Kanwal12345678@users.noreply.github.com>
2024-03-29 02:10:44 +0500

    cli based ATM machine

---
+++ main.js
@@ -0,0 +1,62 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let myBalance = 12000; //Dollar
+let myPin = 6323;
+console.log(chalk.yellowBright("Welcome to Kanwal's ATM machine...."));
+console.log(chalk.greenBright("Please enter your card to begin: "));
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: chalk.magenta("Enter your 4-digit pin code"),
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log(chalk.blueBright("Your pin code is correct!!! Login successfully...."));
+    console.log(chalk.yellow(`Your Current account balance is ${myBalance}`));
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: chalk.red("Please select the Transaction"),
+            type: "list",
+            choices: ["withdraw", "check balance", "fastcash"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: chalk.magenta("Enter your amount to withdraw"),
+                type: "number",
+            },
+        ]);
+        if (amountAns.amount > myBalance) {
+            console.log(chalk.redBright("Insufficient Balance"));
+        }
+        else {
+            console.log(chalk.magenta(`${amountAns.amount} Withdraw Successfully`));
+            (myBalance -= amountAns.amount);
+            console.log(chalk.green(`your remaining account balance is ${myBalance}`));
+        }
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(chalk.bgCyanBright(`your remaining account balance is ${myBalance}`));
+    }
+    else if (operationAns.operation === "fastcash") {
+        let cash = await inquirer.prompt([
+            {
+                name: "fastcash",
+                message: chalk.greenBright("select your amount"),
+                type: "list",
+                choices: ["1000", "2000", "5000", "10000"],
+            },
+        ]);
+        console.log(chalk.blue(`Transaction Successfull!!!`));
+        if (myBalance -= cash.fastcash)
+            console.log(chalk.cyanBright(`Your remaining Account Balance is ${myBalance}`));
+    }
+}
+else {
+    console.log(chalk.bgRed(`Invalid Pin code... Please enter your correct 4-Digit pin code!!`));
+}
